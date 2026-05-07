7bbf5547a06932f49f13ec9cfa7e643f0f15987f
Coro <muhammadcoro22@gmail.com>
2026-05-07 20:34:19 +0700

    commit

--- frontend/src/components/AIChatbot/AIChatbot.js
+++ frontend/src/components/AIChatbot/AIChatbot.js
@@ -3,7 +3,7 @@ import { useAuth } from '../../context/AuthContext';
 import api from '../../utils/api';
 import { FaTimes, FaPaperPlane, FaSpinner } from 'react-icons/fa';
 
-const LOGO = '/images/logo-klinik.png';
+const LOGO = '/images/AI-logo.png';
 
 const SUGGESTIONS = [
     'Manfaat vitamin c',
